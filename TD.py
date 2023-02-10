85420a4d48d166dd349ec893c9c498d15a313011
Benjamin Milhet <benjamin.milhet@outlook.fr>
2023-02-10 08:50:35 +0000

    le gros global

--- TD.py
+++ TD.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-id_cpt = 0
+id_cpt = [0]
 
 @app.route("/")
 def hello_world():
@@ -12,12 +12,11 @@ def hello_world():
 @app.route("/calculatrice", methods=['POST'])
 def calculatrice():
     # curl -X POST -d "nombre1=3" -d "operateur=*" -d "nombre2=4" http://127.0.0.1:5000/calculatrice
-    global id_cpt
+    #global id_cpt
     recupCalcul = request.form.get('nombre1') + request.form.get('operateur') + request.form.get('nombre2')
-    print(recupCalcul)
     calcul = eval(recupCalcul)
-    res = str(id_cpt) + " : " + str(calcul)
-    id_cpt += 1
+    res = str(id_cpt[0]) + " : " + str(calcul)
+    id_cpt[0] = id_cpt[0] + 1
     return res
 
 @app.route("/getId", methods=['GET'])
