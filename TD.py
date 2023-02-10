80b8a9df2a242bcf1a17a51998d092210764e38c
Benjamin Milhet <benjamin.milhet@outlook.fr>
2023-02-10 08:04:36 +0000

    fonction post calculatrice

--- TD.py
+++ TD.py
@@ -3,6 +3,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "Hello, world!"
+
+@app.route("/calculatrice", methods=['POST'])
+def calculatrice(operateur, nombre1, nombre2):
+    return eval(nombre1 + operateur + nombre2)
