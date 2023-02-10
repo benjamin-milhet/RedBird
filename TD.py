755c2ecff3ab287bf4ba2cff93908766c438aced
Benjamin Milhet <benjamin.milhet@outlook.fr>
2023-02-10 07:57:40 +0000

    correction

--- TD.py
+++ TD.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
 import sys
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	return "Hello, world!"
+    return "Hello, world!"
