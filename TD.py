1755c9a269a7840403a87c15094a2be3cc907d69
Benjamin Milhet <benjamin.milhet@outlook.fr>
2023-02-10 08:06:12 +0000

    main

--- TD.py
+++ TD.py
@@ -11,3 +11,15 @@ def hello_world():
 @app.route("/calculatrice", methods=['POST'])
 def calculatrice(operateur, nombre1, nombre2):
     return eval(nombre1 + operateur + nombre2)
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "check_syntax":
+            print("Build [ OK ]")
+            exit(0)
+        else:
+            print("Passed argument not supported ! Supported argument : check_syntax")
+            exit(1)
+    app.run(debug=True)
