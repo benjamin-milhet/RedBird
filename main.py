b6b94449fb42c23a3a00c3ba5cfb9eaa66f7a205
Benjamin Milhet <benjamin.milhet@outlook.fr>
2023-03-03 21:32:44 +0000

    main avec verification de la syntax

--- main.py
+++ main.py
@@ -19,3 +19,13 @@ def inscription():
     r.set("email." + personne.name, personne.email)
 
     return "Bienvenue " + personne.name + "!"
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		if sys.argv[1] == "check_syntax":
+			print("Build [ OK ]")
+			exit(0)
+		else:
+			print("Passed argument not supported ! Supported argument : check_syntax")
+			exit(1)
+	app.run(debug=True)
