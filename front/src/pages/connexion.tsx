cb4cbc319f0f09561adafa49cc32ba8ad9fea118
Clément <clement.cg15@gmail.com>
2023-03-16 11:12:11 +0100

    docker

--- front/src/pages/connexion.tsx
+++ front/src/pages/connexion.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
 import { Title } from '../component/title';
 import { Input } from '../component/form/input';
 import { Button } from '../component/button';
