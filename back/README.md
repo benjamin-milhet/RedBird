2bf69e4a2156655607eafa54f3d7d6e612460502
Benjamin MILHET <benjamin.milhet@outlook.fr>
2023-03-30 22:31:23 +0200

    Test du code pour la première fois et fix des bugs + requete curl JSON

--- back/README.md
+++ back/README.md
@@ -3,3 +3,6 @@
 [![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
 
 ![alt text](https://github.com/benjamin-milhet/4A_ILC_GHYS_MILHET_CLOUD_COMPUTING/blob/main/images/meme2.jpeg?raw=true)
+
+## Moi après avoir fait le backend et le JSON
+![alt text](https://github.com/benjamin-milhet/4A_ILC_GHYS_MILHET_CLOUD_COMPUTING/blob/main/images/meme3.jpeg?raw=true)
--- back/main.py
+++ back/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import sys
 import redis
 import re
+import json
 
 app = Flask(__name__)
 rUser = redis.Redis(host='localhost', port=6379, db=0)
@@ -10,71 +11,91 @@ rTweet = redis.Redis(host='localhost', port=6379, db=1)
 
 @app.route("/")
 def accueil():
-    return "Bienvenue sur notre reseau social de la mère-patrie!"
+    return "Bienvenue sur notre réseau social de la mère-patrie!"
 
 
 @app.route("/inscription", methods=['POST'])
 def inscription():
+    # curl -X POST -H "Content-Type: application/json; charset=utf-8" --data "{\"nom\":\"Lucas\",\"password\":\"pechakuchaDeMerde\"}" http://localhost:5000/inscription
+
     data = request.get_json()
     nom = data.get('nom')
     password = data.get('password')
 
-    tmp = rUser.get(request.form.get("nom." + nom, nom))
+    tmp = rUser.get("nom." + nom)
     if tmp is not None:
-        return "Le nom d'utilisateur " + nom + " n'est pas dispoible."
+        return "Le nom d'utilisateur " + nom + " n'est pas disponible."
     else:
         rUser.set("nom." + nom, nom)
         rUser.set("password." + nom, password)
-        rUser.set("tweet." + nom, [])
+        rUser.set("tweet." + nom, json.dumps([]))
         return "Bienvenue " + nom + "!"
+
 
 @app.route("/tweeter", methods=['POST'])
 def tweeter():
-    # curl -X POST -d "nom=Lucas" -d "tweet=Salut l'élite, c'est El Pueblo, 18-25, 2 sucres #gange #pizza7Fromage" http://localhost:5000/tweeter
+    # curl -X POST -H "Content-Type: application/json; charset=utf-8" --data "{\"nom\":\"Lucas\",\"tweet\":\"Salut l'elite, c'est El Pueblo, 18-25, 2 sucres #gange #pizza7Fromage\"}" http://localhost:5000/tweeter
 
     data = request.get_json()
     nom = data.get('nom')
     tweet = data.get('tweet')
 
-    liste_tweet = rUser.get(request.form.get("tweet." + nom))
-    taille = len(rTweet.keys())
-    liste_tweet[taille] = taille
+    liste_tweet = json.loads(rUser.get("tweet." + nom))
+    taille = len(liste_tweet)
+    liste_tweet.append(taille)
 
-    liste_sujet = chercherHashtag(tweet)
+    liste_sujet = chercher_hashtag(tweet)
     for i in range(len(liste_sujet)):
         liste_sujet[i] = liste_sujet[i].lower()
-        rTweet.set("sujet." + liste_sujet[i], {nom, taille})
+        rTweet.set("sujet." + liste_sujet[i], nom + "." + str(taille))
 
-    rUser.set("tweet." + nom, liste_tweet)
+    rUser.set("tweet." + nom, json.dumps(liste_tweet))
     rTweet.set("tweet." + str(taille), tweet)
 
-    return True
+    return "True"
 
 
 @app.route("/getAllTweets", methods=['GET'])
-def getAllTweets():
+def get_all_tweets():
+    # curl -X GET http://localhost:5000/getAllTweets
+
     liste_tweet = []
-    for i in range(0, len(rTweet.keys())):
-        liste_tweet.append(rTweet.get("tweet." + str(i)))
+
+    for i in range(0, len(rTweet.keys("tweet.*"))):
+        liste_tweet.append(rTweet.get("tweet." + str(i)).decode('utf-8'))
 
     return liste_tweet
 
 
 @app.route("/getAllTweetsByUser", methods=['POST'])
-def getAllTweetsByUser():
+def get_all_tweets_by_user():
+    # curl -X POST -H "Content-Type: application/json; charset=utf-8" --data "{\"nom\":\"Lucas\"}" http://localhost:5000/getAllTweetsByUser
+
     data = request.get_json()
     nom = data.get('nom')
-    
-    liste_tweet = rUser.get(request.form.get("tweet." + nom))
+
+    liste_tweet = json.loads(rUser.get(("tweet." + nom)))
     liste_tweet_final = []
-    for i in range(0, len(liste_tweet)):
-        liste_tweet_final.append(rTweet.get("tweet." + str(i)))
+
+    for i in range(len(liste_tweet)):
+        liste_tweet_final.append(rTweet.get("tweet." + str(liste_tweet[i])).decode('utf-8'))
 
     return liste_tweet_final
 
-def chercherHashtag(tweet):
-    return re.findall(r"#(\w+)", tweet)
 
+@app.route("/getAllSujet", methods=['GET'])
+def get_all_sujet():
+    # curl -X GET http://localhost:5000/getAllSujet
+
+    liste_res = []
+    tmp = rTweet.keys("sujet.*")
+    for i in range(len(tmp)):
+        liste_res.append(tmp[i].decode('utf-8')[6:])
+    return liste_res
+
+
+def chercher_hashtag(tweet):
+    return re.findall(r"#(\w+)", tweet)
 
 
 if __name__ == '__main__':
