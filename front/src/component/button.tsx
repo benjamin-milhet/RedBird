d1692f9c5e973391ff5fe8e7625994af26243231
clement-ghys <64776760+clement-gh@users.noreply.github.com>
2023-03-30 16:39:39 +0200

    updateBoutton

--- front/src/component/button.tsx
+++ front/src/component/button.tsx
@@ -3,13 +3,17 @@ import "./button.css"
 
 type ButtonProps = {
     content: string,
+    className?: string,
+    onClick?: () => void,
+    style?: React.CSSProperties 
 }
 
 export class Button extends React.Component<ButtonProps> {
 
     render(): React.ReactNode {
+       
         return (
-            <button className="button_component" type="button">{this.props.content}</button>
+            <button className={`button_component ${this.props.className}`} onClick={this.props.onClick} type="button" style={this.props.style} >{this.props.content} </button>
         );
     }
 }
