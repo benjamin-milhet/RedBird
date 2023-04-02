64895eb249b9c533dd91ae2349dcc806c8f0ebfe
Clément <clement.cg15@gmail.com>
2023-04-02 12:00:21 +0200

    inscription fonctionnelle

--- front/src/pages/inscription.tsx
+++ front/src/pages/inscription.tsx
@@ -6,32 +6,47 @@ import { Button } from '../component/button';
 import './inscription.css';
 
 export class Inscription extends React.Component{
+    state = {
+        username: "",
+        password: "",
+    };
 
+  handleSignUp = async () => {
+    const response = await fetch("http://localhost:5000/inscription", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            nom: this.state.username,
+            password: this.state.password,
+        }),
+    });
+    
+    const data = await response.json();
+    alert(data.message);
+   if (response.status === 200) {
+        //redirection vers la page de connexion
+        window.location.href = "http://localhost:3000/";
+        localStorage.setItem('username', this.state.username);
+
+    } else {
+        console.log("inscription échouée");
+    }
+
+    };
+
+    handleInputChange = (event:any) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+        
+    };
    
- handleSignUp = () => {
-        const username = (document.getElementById("username") as HTMLInputElement).value;
-        const password = (document.getElementById("password") as HTMLInputElement).value;
-        fetch('http://localhost:3000/signup', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password
-            })
-        }).then((response) => {
-            if (response.status === 200) {
-                console.log("Inscription réussie");
-            } else {
-                console.log("Erreur lors de l'inscription");
-            }
-        });
 
- }
 
 
     render(){
+
         return (
          <main>
             <div className="inscription">
@@ -39,9 +54,9 @@ export class Inscription extends React.Component{
                 <Title content="Inscription"/>
                 <div className="inscription_div">
                     <form className="form_signup">
-                        <Input label="Username"/>
+                        <Input label="Username" name="username" onChange={this.handleInputChange} />
                        
-                        <Input label="Password"/>
+                        <Input label="Password" name="password" onChange={this.handleInputChange} type="password"/>
                        <Button content="S'inscrire" onClick={this.handleSignUp}/>
                     </form>                        
                 </div>
